6800205a316b92ccf6f03847474a8abab9044b3e
Aizada <aizada.omarhan01@gmail.com>
2025-09-17 16:49:50 +0500

    Поменяла auth,routes

--- routes/products.js
+++ routes/products.js
@@ -7,14 +7,20 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/productController.js";
+
 import auth from "../middleware/auth.js";
+import isAdmin from "../middleware/isAdmin.js";
 
 const router = express.Router();
 
+// Публичные (для всех пользователей)
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
-router.post("/", auth, createProduct);
-router.put("/:id", auth, updateProduct);
-router.delete("/:id", auth, deleteProduct);
+
+// Только для админа
+router.post("/", auth, isAdmin, createProduct);
+router.put("/:id", auth, isAdmin, updateProduct);
+router.delete("/:id", auth, isAdmin, deleteProduct);
 
 export default router;
+
